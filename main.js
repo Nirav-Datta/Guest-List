7760b768a7f2cf70c43b9541341d5b5cf784cbc8
Nirav Datta <78023371+Nirav-Datta@users.noreply.github.com>
2021-05-08 14:13:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var names_of_guests_array = [];
+function submit(){
+    names_of_guests_array.push(document.getElementById(name_input).value);
+    document.getElementById("l1").innerHTML=names_of_guests_array;
+    
+}
+function show(){
+    var length_of_guests_array = names_of_guests_array.length;
+    var display_guests_array = [];
+    for(var k = 0; k < length_of_guests_array; k++ ){
+        display_guests_array.push(names_of_guests_array[k])
+    }
+    var remove_commas = display_guests_array.join(" ")
+    document.getElementById("l2").innerHTML = remove_commas;
+    document.getElementById("sorted_list").innerHTML = remove_commas;
+}
+function sorting(){
+    var sorted_list = remove_commas.sort();
+    document.getElementById("sorted_list").innerHTML = sorted_list;
+}
+function search(){
+    var search = document.getElementById("search_input").value;
+    var found = 0;
+    var j;
+    for(j=0; j<length_of_guests_array; j++){
+        if(search==names_of_guests_array[j]){
+            found=found+1;
+        }
+    }
+    document.getElementById("search_result").innerHTML = "name found"+found+"time/s";
+}
